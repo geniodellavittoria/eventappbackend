22c225f7cffe6fd15c9fdbe95b519ff04127be9e
Pascal Keusch <pascal.keusch@stud.hslu.ch>
2019-03-07 18:33:53 +0100

    Add swagger API

--- src/main/java/ch/mobro/eventapp/config/SecurityConfiguration.java
+++ src/main/java/ch/mobro/eventapp/config/SecurityConfiguration.java
@@ -21,21 +21,6 @@ import static org.springframework.http.HttpMethod.POST;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
-    /*
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests().anyRequest().anonymous();
-        http.antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/login**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin().disable();
-    }*/
-
     final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
@@ -50,7 +35,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //@formatter:off
         http
                 .httpBasic().disable()
                 .csrf().disable()
@@ -58,24 +42,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/error").permitAll()
-                //.antMatchers(HttpMethod.GET, "/vehicles/**").permitAll()
-                //.antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN")
-                //.antMatchers(HttpMethod.GET, "/v1/vehicles/**").permitAll()
+                .antMatchers("/", "/csrf", "/v2/api-docs", "/swagger-resources/configuration/ui",
+                        "/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security",
+                        "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
-        //@formatter:on
     }
 
 
-    /*
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-        http.authorizeRequests().antMatchers("/register", "/login").permitAll()
-                .anyRequest().authenticated().and().formLogin();
-    }
-    */
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
